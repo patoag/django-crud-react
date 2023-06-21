0368781f74f351e87c1da16856e988c7f614ff2a
patoag <pantinao.guairao@gmail.com>
2023-06-21 01:25:34 -0400

    implementación de add desde el cliente

--- client/src/api/task.api.js
+++ client/src/api/task.api.js
@@ -1,8 +1,20 @@
 import axios from 'axios';
 
+const taskApi = axios.create({
+    baseURL: 'http://localhost:8000/base/api/v1/tasks/'
+});
+
 export const getAllTasks = async () => {
     try {
-        return axios.get('http://localhost:8000/base/api/v1/tasks/');
+        return taskApi.get('/');
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const createTask = async (task) => {
+    try {
+        return taskApi.post('/', task);
     } catch (error) {
         console.log(error);
     }
--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -1,6 +1,39 @@
+import { useForm } from 'react-hook-form'
+import { createTask } from '../api/task.api'
+import { useNavigate } from 'react-router-dom'
 
 export function TaskFormPage() {
-  return (
-    <div>Task form page</div>
-  )
+	const { register, handleSubmit, formState: {
+        errors
+    } } = useForm()
+
+    // Yup Y Zod son opciones para mejorar las valiadaciones
+
+    const navigate = useNavigate()
+
+    const onSubmit = handleSubmit(async data => {
+        await createTask(data)
+        navigate('/tasks')
+    })
+
+	return (
+		<div>
+			<form onSubmit={onSubmit}>
+				<input
+					type="text"
+					placeholder="title"
+					{...register('title', { required: true })}
+				/>
+                {errors.title && <span>title is required</span>}
+				<textarea
+					rows="3"
+					placeholder="description"
+                    {...register('description', { required: true })}
+				></textarea>
+                {errors.description && <span>description is required</span>}
+
+				<button>Create</button>
+			</form>
+		</div>
+	)
 }
